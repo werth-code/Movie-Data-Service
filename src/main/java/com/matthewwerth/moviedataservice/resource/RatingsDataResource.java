f4ae489eba41ca0b181871d51e518bd5e5c790ea
Matt Werth <matthewwerth@gmail.com>
2021-02-11 13:07:17 -0500

    Connecting Working

--- src/main/java/com/matthewwerth/moviedataservice/resource/RatingsDataResource.java
+++ src/main/java/com/matthewwerth/moviedataservice/resource/RatingsDataResource.java
@@ -2,9 +2,11 @@ package com.matthewwerth.moviedataservice.resource;
 
 import com.matthewwerth.moviedataservice.models.Rating;
 import com.matthewwerth.moviedataservice.models.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,6 +14,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingsDataResource {
+
+//    @Autowired
+//    private RestTemplate restTemplate;
 
     @RequestMapping("/{movieID}")
     public Rating getRating(@PathVariable("movieID") String movieID) {
